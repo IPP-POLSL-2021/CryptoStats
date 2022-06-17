71865105861975dba77a6164587cdb3348e13348
IreQ6 <56777319+IreQ6@users.noreply.github.com>
2022-06-18 01:26:18 +0200

    template for future code iterations

---
+++ main/main/main.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+#include <string>
+
+
+#include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
+
+// for convenience
+using json = nlohmann::json;
+
+
+
+
+int main()
+{
+
+
+    cpr::Response r = cpr::Get(cpr::Url{ "https://api.zonda.exchange/rest/trading/ticker/BTC-PLN" });
+    //cpr::Parameters{ {"anon", "true"}, {"key", "value"} });
+
+    r.status_code;                  // 200
+    r.header["content-type"];       // application/json; charset=utf-8
+    r.text;                         // JSON text string
+
+    auto jason_object = json::parse(r.text);
+
+    std::cout << jason_object.dump(4) << std::endl;
+
+    auto v1 = jason_object["ticker"]["highestBid"].get<std::string>();
+    std::cout << v1 << std::endl;
+    return 0;
+
+}
+
+
